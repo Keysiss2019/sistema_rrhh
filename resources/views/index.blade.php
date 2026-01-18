a81b0f9bfa18921d4e657a6a8a6505c0ec0573e1
Keysiss2019 <keysiss2019@gmail.com>
2026-01-17 20:57:54 -0600

    Crear.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+{{-- 
+ Vista index (Dashboard / Página principal)
+|--}}
+@extends('layouts.app')
+
+{{-- 
+  Sección de contenido principal
+|--}}
+@section('content')
+
+    {{-- Contenido principal pendiente de implementación --}}
+
+@endsection
+
+
