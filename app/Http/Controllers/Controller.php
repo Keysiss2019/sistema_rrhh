031c5a6594d05c0662a5e26a5f9a50c56820dcc2
Keysiss2019 <keysiss2019@gmail.com>
2026-01-17 20:36:48 -0600

    Añadir comentarios.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,24 @@
 <?php
 
-namespace App\Http\Controllers;
+/*
+ Namespace del Controlador Base
+*/
+namespace App\Http\Controllers; //Define la ubicación lógica del controlador dentro de la aplicación.
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Routing\Controller as BaseController;
+/*
+ Importación de Traits y clases base
+*/
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;   // Manejo de autorizaciones (políticas y gates)
+use Illuminate\Foundation\Validation\ValidatesRequests;     // Funcionalidades de validación de formularios
+use Illuminate\Routing\Controller as BaseController;        // Controlador base del framework Laravel
 
+/*
+ Clase Controller
+*/
 class Controller extends BaseController
 {
+    /*
+     Traits utilizados por el controlador
+    */
     use AuthorizesRequests, ValidatesRequests;
 }
