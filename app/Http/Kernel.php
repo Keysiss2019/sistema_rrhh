a92818af59743f422dfe9398e27137530cadfa04
Keysiss2019 <keysiss2019@gmail.com>
2026-01-19 10:07:46 -0600

    Crear

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -36,7 +36,8 @@ class Kernel extends HttpKernel
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            \App\Http\Middleware\ForcePasswordChange::class,
+            \App\Http\Middleware\CheckPasswordChange::class,
+            \App\Http\Middleware\ForcePasswordChange::class
         ],
 
         'api' => [
